47965cd04d081a1b2aa67067694f02f82689cf14
INO <bih0905@naver.com>
2023-10-18 22:40:28 +0900

    231018 request json servlet

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,20 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * description :
+ * fileName    : HelloData
+ * author      : ino
+ * since       : 2023-10-18
+ */
+
+@Getter
+@Setter
+public class HelloData {
+
+  private String username;
+  private int age;
+
+}
